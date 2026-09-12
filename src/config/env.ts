7d16f2393082934297ea6521539999cba53efda2
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-09-12 12:00:50 +0530

    added changes

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-    port: process.env.PORT || 4000,
+    port: Number(process.env.PORT || 4000),
     jwtSecret: process.env.JWT_SECRET!,
 
 
