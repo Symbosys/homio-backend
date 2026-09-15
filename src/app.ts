442f81d10429798f5f16212b5d26e5339221a7ba
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-09-15 14:09:57 +0530

    implemented server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { ENV } from "./config/env.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import authRoutes from "./module/user/routes/auth.routes.js";
@@ -6,6 +7,7 @@ import userRoutes from "./module/user/routes/user.routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
